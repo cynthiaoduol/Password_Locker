41796b448d18c078ce0b1a1d80e51a7eb598fb09
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-07 05:04:35 +0300

    add a tearDown method to clean up after each test case has run.

--- locker_test.py
+++ locker_test.py
@@ -45,6 +45,12 @@ class TestCredentials(unittest.TestCase):
         '''
         self.new_credential = Credentials("Gmail","dorydory","143811jsa")
 
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
+
     def test_init(self):
         '''
         Test case to check if a new Credentials instance has been initialized correctly
@@ -52,6 +58,13 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.account,"Gmail")
         self.assertEqual(self.new_credential.userName,"dorydory")
         self.assertEqual(self.new_credential.password,"143811jsa") 
+
+    def save_credential_test(self):
+        '''
+        test case to test if the credential object is saved into the credentials list.
+        '''
+        self.new_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),1)
 
     
 
