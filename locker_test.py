dbfade2cd7ecdca99bff65858a95ded55b7d68ab
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-07 04:14:52 +0300

    test if a new user object has been saved into the User list

--- locker_test.py
+++ locker_test.py
@@ -15,4 +15,22 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("FeistyDory","21038")
 
-    
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,'FeistyDory')
+        self.assertEqual(self.new_user.password,'21038')
+
+
+    def test_save_user(self):
+        '''
+        test case to test if a new user object has been saved into the User list
+        '''
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+if __name__ == "__main__":
+    unittest.main() 
