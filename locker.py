45e3cc63d2731718539e736318c866fabf6b660e
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-07 11:30:52 +0300

    import random to help in random generation of passwords

--- locker.py
+++ locker.py
@@ -87,6 +87,8 @@ class Credentials():
 
 
     def generatePassword(stringLength=8):
-        """Generate a random password string of letters and digits and special characters"""
+        '''
+        Generate a random password string of letters and digits and special characters
+        '''
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
         return ''.join(random.choice(password) for i in range(stringLength)) 
