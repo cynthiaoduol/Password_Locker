d108af9da0e51f1e8156fdc2dca12a391faeb7e5
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-07 03:24:24 +0300

    create the _init method that defines the properties for user objects

--- locker.py
+++ locker.py
@@ -1,5 +1,14 @@
 class User:
     '''
-    Create User class that generates new instances of a user.
+    Create User class that generates new instances of a user
     '''
-    
+    user_list = []
+
+    def __init__(self,username, password):
+        '''
+        method that defines the properties for user objects
+        '''
+        self.username = username
+        self.password = password 
+
+        
